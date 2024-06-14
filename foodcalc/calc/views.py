a3d13ac2e01c3da1ecbf8d43f43c2fb639076fa3
EduardKiselev <e.kiselev88@gmail.com>
2024-06-14 21:24:18 +0700

    Lena privet

--- foodcalc/calc/views.py
+++ foodcalc/calc/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from calc.models import Food, NutrientsName, NutrientsQuantity
 from django import forms
-from itertools import chain
 
 
 class FoodForm(forms.Form):
@@ -11,6 +10,7 @@ class FoodForm(forms.Form):
 class RemoveFoodForm(forms.Form):
     remove_food = forms.CharField(max_length=150)
 
+
 class MassForm(forms.Form):
     mass = forms.IntegerField()
 
@@ -19,18 +19,17 @@ def index(request, chosen_food=[], mass_dict={}):
 
     if request.GET:
         if 'food' in request.GET:
-            form = FoodForm(request.GET)
-           # print(form)
-      #      print('---------------------------GET', request.GET['food'],request)
+            form = FoodForm(request.GET or None)
+
             if form.is_valid():
-       #         print('111111',chosen_food)
+
                 food_to_add = form.cleaned_data['food']
-      #          print('food_to_add', food_to_add)
-                if food_to_add not in chosen_food:
-                    chosen_food.append(food_to_add)
-                    food_class = get_object_or_404(Food, description=food_to_add)
-                    mass_dict[food_class] = 100
-                    print('22222',type(food_class))
+                food_class = get_object_or_404(Food, description=food_to_add)
+                if food_class.id not in chosen_food:
+                    chosen_food.append(food_class.id)
+                    print('ADDD111111', chosen_food)
+                    mass_dict[food_class.id] = 100
+               # print('22222',type(food_class))
 
         elif 'remove_food' in request.GET:
             remove_form = RemoveFoodForm(request.GET)
@@ -39,68 +38,58 @@ def index(request, chosen_food=[], mass_dict={}):
        #         print('3333333',chosen_food)
                 food_to_remove = remove_form.cleaned_data['remove_food']
       #          print('food_to_remove', food_to_remove)
-                index = chosen_food.index(food_to_remove)
+                id = get_object_or_404(Food,description=food_to_remove).id
+                index = chosen_food.index(id)
       #          print(index)
                 chosen_food.pop(index)
-                del mass_dict[food_to_remove]
-
+                del mass_dict[id]
+        del request.GET
 
     
     if request.POST:
         print('!!!!POST!!!!', request.POST.keys())
-        for food in chosen_food:
-            if 'mass_'+food in request.POST.keys() and request.POST['mass_'+food]:
-                mass = int(request.POST['mass_'+food])
+        for food_id in chosen_food:
+            if 'mass_'+str(food_id) in request.POST.keys() and request.POST['mass_'+str(food_id)]:
+                mass = int(request.POST['mass_'+str(food_id)])
                 if mass > 0:
-                    mass_dict[food] = mass
-                print('TYPEE!!!!!', food, type(mass_dict[food]))
-    print('mass_dict:', mass_dict)
+                    mass_dict[food_id] = mass
+                print('TYPEE!!!!!', str(food_id), type(mass_dict[food_id]))
+    print('mass_dict:', mass_dict,'\n','chosen_food:',chosen_food)
 
-    foodlist = Food.objects.exclude(description__in=chosen_food)
+    foodlist = Food.objects.exclude(id__in=chosen_food)
+    print(foodlist)
     form = FoodForm()
     delete_list = []
     nutrients = []
     items_name = []
+    totals = {}
     if chosen_food:
-        delete_list = Food.objects.filter(description__in=chosen_food)
+        delete_list = Food.objects.filter(id__in=chosen_food)
         for elem in chosen_food:
-            item = [NutrientsQuantity.objects.select_related('food', 'nutrient').filter(food__description=elem, nutrient__is_published=True)]
-            
-
-            
-            print('ITEMS\n\n',item[0].values('nutrient_id', 'amount'))
-
-        #    for elem in item[0].values('food_id', 'nutrient_id', 'amount'):
-       #        print(elem)
-          
+            item = [NutrientsQuantity.objects.select_related('food', 'nutrient').filter(food__id=elem, nutrient__is_published=True)]
+            print('ITEMS\n\n', item[0].values('nutrient_id', 'amount'))
+            print('~~~~~~~~~~~~~',item[0].values('nutrient_id'),item[0].values('amount'))
             nutrients += item
-            item_name = get_object_or_404(Food, description=elem)
+            item_name = get_object_or_404(Food, id=elem)
             items_name += [item_name]
-        #     item_name = Food.objects.filter(description=elem)
-        #     items.append(item_name)
-        #     item = NutrientsQuantity.objects.filter(food__description=elem, nutrient__is_published=True).order_by('nutrient__name')
-            
-        # #    print('elem', elem, '\nITEM:', item.values(), '\n')
 
-            # items.append(chain(item_name, item))
-   #     print('ITEMS!!!!!',items,'\n','ITEMS_NAME!!!',items_name)
+
+
     columns = NutrientsName.objects.filter(is_published=True).order_by('name')
-  #  print('\COLUMNS:', columns.values(), '\n')
+    print('\COLUMNS:', columns.values(), '\n')
+    for food_id in chosen_food:
+        all_nutr = NutrientsQuantity.objects.select_related('nutrient').filter(food__id=food_id, nutrient__is_published=True)
+        for nutr in all_nutr.iterator():
+            print('nutrient', nutr.nutrient, type(nutr.nutrient.name))
+            totals[nutr.nutrient.name] = round(totals.get(nutr.nutrient.name, 0) + mass_dict[food_id]*nutr.amount/100,2)
+    print('TOTASLS', totals)
     for elem in mass_dict:
         print(type(elem))
     print('CHOSEN FOOD', chosen_food)
-    mass_dict['Frankfurter, beef, unheated']=10000
-    mass_dict['1']=-10000
-    context = {"form": form, "showfood": foodlist, 'chosen': chosen_food,'mass_dict': mass_dict}
+    context = {"form": form, "showfood": foodlist, "mass_dict": mass_dict}
     if chosen_food:
-        context |= {'delete_list': delete_list, 'columns': columns, 'items': nutrients,'items_name': items_name}
-        print('ITEMS NAME[0]:', items_name[0].description, type(items_name[0].description))
-        for i in range(len(items_name)):
-            print(mass_dict[items_name[i]])
-            print(type(items_name[i]))
-
-
-
+        context |= {'delete_list': delete_list, 'columns': columns, 'items': nutrients,'items_name': items_name,'totals':totals}
+        print('ITEMS NAME[0]:', items_name[0].id)
 
     print("form:", form, '\ncontext[mass_dict]:', context['mass_dict'], '\n')
     return render(request, template, context)
