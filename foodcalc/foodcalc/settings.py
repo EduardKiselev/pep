a0b388fca6e3a28cb3a3df5b74744729b3178edd
EduardKiselev <e.kiselev88@gmail.com>
2026-06-13 06:24:02 +0300

    new server

--- foodcalc/foodcalc/settings.py
+++ foodcalc/foodcalc/settings.py
@@ -42,7 +42,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',docker volume rm pep_postgres_data
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
